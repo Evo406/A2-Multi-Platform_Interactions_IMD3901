faea7435c7250477285f248ad42d9c85699ddb82
Evo406 <everett.ohman@gmail.com>
2024-02-04 19:41:21 -0500

    added object instantiation

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
-//this is our javascript web server
-const express   = require("express");       //require == #include
+const express   = require('express');
 const app       = express();
-const http      = require("http");
+const http      = require('http');
 const server    = http.createServer(app);
+const io        = require('socket.io')(server);
 
-// To run program in terminal, type 'node .\app.js
-const LISTEN_PORT       = 8080;           //default port 80
-const ABS_STATIC_PATH   = __dirname + '/public';
+const LISTEN_PORT = 8080;
 
+// middleware to serve static files
+app.use(express.static(__dirname + '/public;'));
 
-//set our routes
-app.get('/', function (req, res) {
-    res.sendFile('index.html', {root:ABS_STATIC_PATH});
+//create our routes
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-server.listen(LISTEN_PORT);                         //starts server
-app.use(express.static(__dirname + '/public'));     //the client can access these files via http
-console.log("Listening on port: " + LISTEN_PORT);   //a console output so we know something is happening
+app.get('/2d', function(req,res) {
+    res.sendFile(__dirname + '/public/2d.html');
+});
+
+app.get('/3d', function(req,res) {
+    res.sendFile(__dirname + '/public/3d.html');
+});
+
+//websocket stuff
+io.on('connection', (socket) => {
+    console.log(socket.id + " is connected");
+
+    socket.on('disconnect', () =>{
+        console.log(socket.id + " is disconnected");
+    })
+
+    // custom events
+    socket.on('red', (data) => {
+        console.log("poop recieved! Wow!");
+        io.emit('color_change', {r:255, g:0, b:0}); // send an event to al clients (see cheet sheet online)
+    });
+
+    socket.on('blue', (data) => {
+        console.log("Blue event recieved! Wow!");
+        io.emit('color_change', {r:0, g:0, b:255}); // send an event to al clients (see cheet sheet online)
+    });
+});
+
+//start our server
+server.listen(LISTEN_PORT);
+console.log("Server started on port " + LISTEN_PORT);
