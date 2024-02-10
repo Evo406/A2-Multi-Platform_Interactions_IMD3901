7c3ca3daf1e3240630ce4870203f50de37a1db82
Evo406 <everett.ohman@gmail.com>
2024-02-10 13:32:50 -0500

    removed references to socket io

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express   = require('express');
 const app       = express();
 const http      = require('http');
 const server    = http.createServer(app);
-const io        = require('socket.io')(server);
 
 const LISTEN_PORT = 8080;
 
@@ -18,25 +17,6 @@ app.get('/space', function(req,res) {
     res.sendFile(__dirname + '/public/space.html');
 });
 
-//websocket stuff
-io.on('connection', (socket) => {
-    console.log(socket.id + " is connected");
-
-    socket.on('disconnect', () =>{
-        console.log(socket.id + " is disconnected");
-    })
-
-    // custom events
-    socket.on('red', (data) => {
-        console.log("poop recieved! Wow!");
-        io.emit('color_change', {r:255, g:50, b:50}); // send an event to al clients (see cheet sheet online)
-    });
-
-    socket.on('blue', (data) => {
-        console.log("Blue event recieved! Wow!");
-        io.emit('color_change', {r:50, g:50, b:255}); // send an event to al clients (see cheet sheet online)
-    });
-});
 
 //start our server
 server.listen(LISTEN_PORT);
