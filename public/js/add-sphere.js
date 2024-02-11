77b0a4c09b244d4c824a71df56b4192ea58ac2e0
Evo406 <everett.ohman@gmail.com>
2024-02-10 20:41:23 -0500

    fixed planet destroy on mouse click. button now destroys all planets too

--- public/js/add-sphere.js
+++ public/js/add-sphere.js
@@ -7,7 +7,6 @@ AFRAME.registerComponent('add-sphere', {
         CONTEXT_AF.button = document.querySelector('#add_button');
         CONTEXT_AF.scene = document.querySelector('a-scene');
         CONTEXT_AF.planets = document.querySelectorAll('.planet');
-        let planetLength = CONTEXT_AF.planets.lenth;
 
         CONTEXT_AF.button.addEventListener('click', function() {
             console.log("Creating sphere...");
@@ -23,11 +22,8 @@ AFRAME.registerComponent('add-sphere', {
             entity.setAttribute('position',{x: posX, y: posY, z: -5});
             entity.setAttribute('class', 'interactive planet');
             entity.setAttribute('color','rgb(' + r + ',' + g + ',' + b + ')');
-            // entity.setAttribute('id', planetLength + 1);
-            //entity.setAttribute('animation__mouseenter', 'property:material.color; type:color; to:rgb(230,230,230); startEvents:mouseenter; dur:200;')
-            //entity.setAttribute('animation__mouseleave', 'property:material.color; type:color; to:rgb(' + r + ',' + g + ',' + b + '); startEvents:mouseleave; dur:200;')
             entity.setAttribute('animation__click', 'startEvents:click;')
-            entity.setAttribute('planet-destroy-selected'); 
+            entity.setAttribute('planet-destroy-selected', ''); 
 
             CONTEXT_AF.scene.appendChild(entity);
             console.log("Sphere created!");
--- public/js/planet-destroy-selected.js
+++ public/js/planet-destroy-selected.js
@@ -4,31 +4,13 @@ AFRAME.registerComponent('planet-destroy-selected', {
     init:function() {
 
         const CONTEXT_AF = this;
-        CONTEXT_AF.button = document.querySelector('#del_button');
-        CONTEXT_AF.planets = document.querySelectorAll('.planet');
-
         // Attach a click event listener to the entity
         CONTEXT_AF.el.addEventListener('click', function() {
-            //console.log("Destroying planet: " + CONTEXT_AF.el.getAttribute('id'));
-
             // Remove the planet entity from the scene
+            console.log('planet clicked');
             CONTEXT_AF.el.remove();
         });  
 
-        // CONTEXT_AF.planets[0].addEventListener('click', function() {
-        //     console.log("clicked on a planet!");
-        // });
-        
-
-        // Delete all planets from the scene by pressing the red button
-        CONTEXT_AF.button.addEventListener('click', function() {
-                let planets = document.querySelectorAll('.planet');
-                for(let i=0; i<planets.length; i++){
-                    //planets.parentNode.removeChild(planets[i]);
-                    console.log('removed planet:' + planets[i] + i);
-                    planets[i].remove();
-                }
-            });
     }
 });
 
